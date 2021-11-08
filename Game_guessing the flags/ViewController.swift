8453efe995e9fb8c083b26a88920783b4c8ab901
Kim - leo <ochell2@naver.com>
2021-11-08 18:25:07 +0900

    Adding the pictures of flags, Making buttons on the screen

--- Game_guessing the flags/ViewController.swift
+++ Game_guessing the flags/ViewController.swift
@@ -7,11 +7,22 @@
 
 import UIKit
 
+
+
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet var imgBtn1: UIButton!
+    @IBOutlet var imgBtn2: UIButton!
+    @IBOutlet var imgBtn3: UIButton!
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //title = countries.shuffle()
     }
 
 
