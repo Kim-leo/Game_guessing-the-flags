363c955bf0a5ed37e189469975003d7c9ca37167
Kim - leo <ochell2@naver.com>
2021-11-08 18:40:43 +0900

    Making the border line of each buttons, and Put images into the buttons

--- Game_guessing the flags/ViewController.swift
+++ Game_guessing the flags/ViewController.swift
@@ -7,22 +7,32 @@
 
 import UIKit
 
-
-
-
-
 class ViewController: UIViewController {
 
     @IBOutlet var imgBtn1: UIButton!
     @IBOutlet var imgBtn2: UIButton!
     @IBOutlet var imgBtn3: UIButton!
     
-   
-    
+    var score = 0
+    var countries = ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        //title = countries.shuffle()
+  
+        imgBtn1.layer.borderWidth = 1
+        imgBtn1.layer.borderColor = UIColor.black.cgColor
+        imgBtn2.layer.borderWidth = 1
+        imgBtn2.layer.borderColor = UIColor.black.cgColor
+        imgBtn3.layer.borderWidth = 1
+        imgBtn3.layer.borderColor = UIColor.black.cgColor
+        
+        button()
+    }
+    
+    func button(action: UIAlertAction! = nil) {
+        imgBtn1.setImage(UIImage(named: countries[0]), for: .normal)
+        imgBtn2.setImage(UIImage(named: countries[1]), for: .normal)
+        imgBtn3.setImage(UIImage(named: countries[2]), for: .normal)
     }
 
 
