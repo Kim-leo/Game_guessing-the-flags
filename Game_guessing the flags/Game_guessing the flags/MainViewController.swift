3b13498e412ebb9653bc3fc63904f4b5037045ae
Kim - Leo <77371366+Kim-leo@users.noreply.github.com>
2021-11-16 22:06:37 +0900

    Add files via upload
    Making a game "Guessing the flags" has been completed.
    Sharing button on the main screen is added.
    Players can share their score with others.

---
+++ Game_guessing the flags/Game_guessing the flags/MainViewController.swift
@@ -0,0 +1,55 @@
+//
+//  MainViewController.swift
+//  Game_guessing the flags
+//
+//  Created by 김승현 on 2021/11/08.
+//
+
+import UIKit
+
+class MainViewController: UIViewController, sendDataDelegate {
+    @IBOutlet weak var lastScore: UILabel!
+    
+    func updateLabelText(text: String) {
+        lastScore.text = "Last Score : " + text
+    }
+     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = ""
+        let backBarButtonItem = UIBarButtonItem(title: "Go to Main", style: .plain, target: self, action: nil)
+        self.navigationItem.backBarButtonItem = backBarButtonItem
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareBtn))
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToNext" {
+            let vc = segue.destination as! ViewController
+            vc.del = self
+        }
+    }
+    
+    @objc func shareBtn() {
+        var shareObject = [Any]()
+        shareObject.append(lastScore.text!)
+        
+        let ac = UIActivityViewController(activityItems: shareObject, applicationActivities: nil)
+        present(ac, animated: true)
+    }
+}
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
